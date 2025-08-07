46861b3ece08ccf299030d14d1f51c5f989436bd
Muhammad Shoaib Khan <imkhanshoaib21@gmail.com>
2025-08-07 21:33:46 +0530

    dashboard with solo requests

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,55 +1,58 @@
-import React from 'react'
-import { Protect,} from "@clerk/clerk-react";
-import {useEffect, useState } from 'react';
-import { dummyCreationData } from '../assets/assets';
-import { Gem, Sparkles } from 'lucide-react';
+import React from "react";
+import { Protect } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import { dummyCreationData } from "../assets/assets";
+import { Gem, Sparkles } from "lucide-react";
+import CreationItem from "../components/CreationItem.jsx";
 
 const Dashboard = () => {
-
   const [creations, setCreations] = useState([]);
   const getDashboardData = async () => {
-    setCreations(dummyCreationData)
-  }
+    setCreations(dummyCreationData);
+  };
 
   useEffect(() => {
     getDashboardData();
   }, []);
 
   return (
-    <div className='h-full overflow-y-scroll p-6'>
-        <div className='flex justify-start gap-4 flex-wrap '>
-          {/* Total Creations card */}
-          <div className='flex justify-between items-center w-72 p-4 bg-white rounded-xl border border-gray-200'>
-            <div className='text-slate-600' >
-              <p className='text-sm'>Total Creations</p>
-              <h2 className='text-xl font-semibold'>{creations.length}</h2>
-            </div>
-            <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center ' >
-              <Sparkles className='w-5 text-white' />
-            </div>
+    <div className="h-full overflow-y-scroll p-6">
+      <div className="flex justify-start gap-4 flex-wrap ">
+        {/* Total Creations card */}
+        <div className="flex justify-between items-center w-72 p-4 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Total Creations</p>
+            <h2 className="text-xl font-semibold">{creations.length}</h2>
           </div>
-
-          {/* Active plan card */}
-          <div className='flex justify-between items-center w-72 p-4 bg-white rounded-xl border border-gray-200'>
-            <div className='text-slate-600' >
-              <p className='text-sm'>Active Plan</p>
-              <h2 className='text-xl font-semibold'>
-                <Protect plan='premium' fallback='Free' >Premium</Protect>
-              </h2>
-            </div>
-            <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center'>
-              <Gem className='w-5 text-white' />
-            </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center ">
+            <Sparkles className="w-5 text-white" />
           </div>
-
         </div>
 
-        <div className='space-y-3' >  
-          <p className='mt-6 mb-4' >Recent Creations</p>
+        {/* Active plan card */}
+        <div className="flex justify-between items-center w-72 p-4 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Active Plan</p>
+            <h2 className="text-xl font-semibold">
+              <Protect plan="premium" fallback="Free">
+                Premium
+              </Protect>
+            </h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center">
+            <Gem className="w-5 text-white" />
+          </div>
         </div>
-      
+      </div>
+
+      <div className="space-y-3">
+        <p className="mt-6 mb-4">Recent Creations</p>
+        {creations.map((item) => (
+          <CreationItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
